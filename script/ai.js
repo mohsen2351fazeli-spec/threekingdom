7f1c6fc6788818314560c93d6b812c54e60bae93
Mohsen Fazeli <mohsen2351fazeli@gmail.com>
2026-09-10 05:26:46 -0700

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -95,7 +95,7 @@ const chatWithGroq = async () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "openrouter/free",
+          model: "qwen/qwen3-30b-a3b:free",
           max_tokens: 2000,
           messages: [
             {
